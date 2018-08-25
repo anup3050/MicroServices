f8f665bf43eb5f3425ca11af36e8bd7269a57403
unknown <anup3050@gmail.com>
2018-08-25 23:34:19 +0530

    versioning using URI

---
+++ src/main/java/com/example/rest/websevices/restfullwebservice/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.example.rest.websevices.restfullwebservice.versioning;
+
+public class PersonV1 {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+}
