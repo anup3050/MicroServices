26a4d1d630220e7852167ac8f4e3674d626ae315
unknown <anup3050@gmail.com>
2018-08-25 13:46:28 +0530

    swagger

--- src/main/java/com/example/rest/websevices/restfullwebservice/User/User.java
+++ src/main/java/com/example/rest/websevices/restfullwebservice/User/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description ="All info about User")
 public class User {
 
 	private Integer userId;
 
-	@Size(min = 3, message = "Name should have three char")
+	@Size(min = 3, message = "Name should have atleast three char")
+	@ApiModelProperty (notes = "Name should have atleast three char")
 	private String name;
 
 	@Past
+	@ApiModelProperty (notes = "DOB should be in past")
 	private Date dob;
 
 	protected User() {
