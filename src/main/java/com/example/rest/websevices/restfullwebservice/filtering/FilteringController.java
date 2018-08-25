3ac5005c92b5d667d08e1641023fb662fd32dc35
unknown <anup3050@gmail.com>
2018-08-25 23:08:28 +0530

    Dynamic json ingore or filtering

--- src/main/java/com/example/rest/websevices/restfullwebservice/filtering/FilteringController.java
+++ src/main/java/com/example/rest/websevices/restfullwebservice/filtering/FilteringController.java
@@ -3,21 +3,40 @@ package com.example.rest.websevices.restfullwebservice.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
 	@GetMapping(path="/filtering")
-	public SomeBean retriveSomeBean() {
+	public MappingJacksonValue retriveSomeBean() {
 		
-		return new SomeBean("Value1","Value2","Value3");
+		SomeBean someBean = new SomeBean("Value1","Value2","Value3");
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field2");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter );
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		
+		return mapping;
 	}
 	
 	@GetMapping(path="/filtering-list")
-	public List<SomeBean> retriveSomeBeanList() {
+	public MappingJacksonValue retriveSomeBeanList() {
 		
-		return Arrays.asList((new SomeBean("Value1","Value2","Value3")),new SomeBean("Value11","Value22","Value33"));
+		List someBeanList = Arrays.asList((new SomeBean("Value1","Value2","Value3")),new SomeBean("Value11","Value22","Value33"));
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter );
+		MappingJacksonValue mapping = new MappingJacksonValue(someBeanList);
+		mapping.setFilters(filters);
+		
+		
+		return mapping;
 	}
 }
--- src/main/java/com/example/rest/websevices/restfullwebservice/filtering/SomeBean.java
+++ src/main/java/com/example/rest/websevices/restfullwebservice/filtering/SomeBean.java
@@ -1,40 +1,41 @@
 package com.example.rest.websevices.restfullwebservice.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
+@JsonFilter("SomeBeanFilter") //its dynamic ignore 
+//@JsonIgnoreProperties({"field1","field2"}) its a optional to @Jsonignor. Its static ignore
 public class SomeBean {
 
-	@JsonIgnore
-	private String value1;
-	private String value2;
-	private String value3;
+	//@JsonIgnore it will ignore below property. Its static ignore
+	private String field1;
+	private String field2;
+	private String field3;
 	public SomeBean(String value1, String value2, String value3) {
 		super();
-		this.value1 = value1;
-		this.value2 = value2;
-		this.value3 = value3;
+		this.field1 = value1;
+		this.field2 = value2;
+		this.field3 = value3;
+	}
+	public String getField1() {
+		return field1;
+	}
+	public void setField1(String field1) {
+		this.field1 = field1;
+	}
+	public String getField2() {
+		return field2;
+	}
+	public void setField2(String field2) {
+		this.field2 = field2;
+	}
+	public String getField3() {
+		return field3;
+	}
+	public void setField3(String field3) {
+		this.field3 = field3;
 	}
 	
 	
-	public String getValue1() {
-		return value1;
-	}
-	public void setValue1(String value1) {
-		this.value1 = value1;
-	}
-	public String getValue2() {
-		return value2;
-	}
-	
-	public void setValue2(String value2) {
-		this.value2 = value2;
-	}
-	public String getValue3() {
-		return value3;
-	}
-	public void setValue3(String value3) {
-		this.value3 = value3;
-	}
-	
+		
 	
 }
