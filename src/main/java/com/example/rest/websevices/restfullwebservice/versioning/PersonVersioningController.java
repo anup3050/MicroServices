1cf886efc2ecfab8dac83f31c7a45d22c096675d
unknown <anup3050@gmail.com>
2018-08-26 17:41:30 +0530

    Versioning

--- src/main/java/com/example/rest/websevices/restfullwebservice/versioning/PersonVersioningController.java
+++ src/main/java/com/example/rest/websevices/restfullwebservice/versioning/PersonVersioningController.java
@@ -17,4 +17,39 @@ public class PersonVersioningController {
 		
 		return new PersonV2(new Name("MS","Dhoni"));
 	}
+	
+	@GetMapping(value = "/person/param", params ="version=1")
+	public PersonV1 paramV1() {
+		
+		return new PersonV1("MS Dhoni");
+	}
+	
+	@GetMapping(value = "/person/param", params ="version=2")
+	public PersonV2 paramV2() {
+		
+		return new PersonV2(new Name("MS","Dhoni"));
+	}
+	
+	@GetMapping(value = "/person/heade", headers ="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		
+		return new PersonV1("MS Dhoni");
+	}
+	
+	@GetMapping(value = "/person/header", headers ="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		
+		return new PersonV2(new Name("MS","Dhoni"));
+	}
+	
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("MS Dhoni");
+	}
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("MS","Dhoni"));
+	}
+
 }
